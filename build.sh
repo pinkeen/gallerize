28dbb57e3624faf806c9a008eca34e79b17667ca
Filip Sobalski <pinkeen@gmail.com>
2014-06-02 21:51:54 +0200

    Added bower.json

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-lessc -O0 --yui-compress less/gallerize.less css/gallerize.css
-yuicompressor js/gallerize.js -o js/gallerize.min.js
+lessc less/gallerize.less css/gallerize.css
+uglifyjs js/gallerize.js > js/gallerize.min.js
 
 ls demo/pictures/*.jpg | while read i; do
     convert "${i}" -resize '150x^150' -gravity center -crop "150x150+0+0 +repage"  "demo/thumbs/$(basename "$i")"
